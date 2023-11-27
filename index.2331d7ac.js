9ed95cf7bb58160582b3fd60e9b1bd3ce8372be3
victoria-gl <138527509+victoria-gl@users.noreply.github.com>
2023-11-27 13:05:19 +0000

    Deploying to gh-pages from @ victoria-gl/tranny-parcel@a0fa00ae6bfda85cd9d0167d48773d0e10082545 🚀

---
+++ index.2331d7ac.js
@@ -0,0 +1,2 @@
+const o=localStorage.getItem("Array of names");console.log(o),console.log(JSON.parse(o));
+//# sourceMappingURL=index.2331d7ac.js.map
---
+++ index.74b9302a.js
@@ -0,0 +1,2 @@
+!function(){var o=localStorage.getItem("Array of names");console.log(o),console.log(JSON.parse(o))}();
+//# sourceMappingURL=index.74b9302a.js.map
