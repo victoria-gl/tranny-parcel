0ffcea7fed4425246f736a15ac63c188f767cc60
victoria-gl <138527509+victoria-gl@users.noreply.github.com>
2023-11-27 17:08:10 +0100

    lessona-8.2

--- src/script.js
+++ src/script.js
@@ -4,8 +4,8 @@
 
 //local Storage
 
-const LS_KEY = "Array of names";
-const names = ["Alice", "Kate", "Emma"];
+// const LS_KEY = "Array of names";
+// const names = ["Alice", "Kate", "Emma"];
 
 
 //Збереження в ls
@@ -16,9 +16,58 @@ const names = ["Alice", "Kate", "Emma"];
 
 //Читання данних в LS
 
-const value = localStorage.getItem(LS_KEY);
-console.log(value);
-console.log(JSON.parse(value));
+// const value = localStorage.getItem(LS_KEY);
+// console.log(value);
+// console.log(JSON.parse(value));
 
 
 //Видалення данних з LS
+
+// localStorage.removeItem(LS_KEY);
+
+
+//Очищення(повне) LS
+
+// localStorage.setItem("lalala", "hello");
+// localStorage.setItem("tuutut", "world");
+
+// localStorage.clear()
+
+
+//Чому варто завжди використовувати метод JSON.stringify
+
+// const LS_KEY = "Array of names";
+// const names = ["Alice", "Kate", "Emma"];
+
+// localStorage.setItem(LS_KEY, names)
+
+// const value = localStorage.getItem(LS_KEY)
+// console.log(value);
+// console.log(JSON.parse(value)); - видасть помилку, бо цей рядок не можливо розпарсити
+
+const LS_KEY = "my function";
+
+// function add (a, b){
+//     return a + b;
+// }
+
+// localStorage.setItem(LS_KEY, add);
+
+// console.log(localStorage.getItem(LS_KEY));
+
+// const foo = JSON.parse(localStorage.getItem(LS_KEY))
+
+//функції в LS не кладеться ні в якому вигляді
+
+
+const calc = {
+    a: 2,
+    b: 3,
+    add(){
+        return this.a + this.b
+    }
+}
+
+localStorage.setItem(LS_KEY, JSON.stringify(calc))
+console.log(JSON.parse(localStorage.getItem(LS_KEY)));
+
