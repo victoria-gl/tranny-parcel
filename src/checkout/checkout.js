35fa6daee80d6301a90bae0558b26f97ad6abf41
victoria-gl <138527509+victoria-gl@users.noreply.github.com>
2023-11-28 16:58:40 +0100

    lesson-8.2

--- src/checkout/checkout.js
+++ src/checkout/checkout.js
@@ -1,28 +1,35 @@
               //Урок 8.2
 
-// import { createMarkup } from "../templates/templateCheckout";
+import { createMarkup } from "../templates/templateCheckout";
 // import { save, load } from "../templates/helpers";
 
-// const selectors = {
-//     container: document.querySelector(".js-list"),
-//     totalPrice: document.querySelector(".js-total-price"),
-//     clear: document.querySelector(".js-btn")
-// }
+// const container = document.querySelector(".js-list")  - класичний варіант запису
+// const totalPrice = document.querySelector(".js-total-price")
+// const clear = document.querySelector(".js-btn")
 
-// const PRODUCT_LS_KEY = 'checkout';
-// const products = JSON.parse(localStorage.getItem(PRODUCT_LS_KEY)) || [];
-// let totalPrice;
+const selectors = {
+    container: document.querySelector(".js-list"),
+    totalPrice: document.querySelector(".js-total-price"),
+    clear: document.querySelector(".js-btn")
+}
 
-// if(products.length) {
-//     selectors.clear.hidden = false;
-//     totalPrice = products.reduce((acc, { qty, price }) => acc += qty * price, 0);
-// }
+const PRODUCT_LS_KEY = 'checkout';
+const products = JSON.parse(localStorage.getItem(PRODUCT_LS_KEY)) || [];
 
-// selectors.totalPrice.textContent = totalPrice ? `Total price: ${totalPrice}` : 'Your basket is empty';
-// selectors.container.insertAdjacentHTML("beforeend", createMarkup(products));
-// selectors.clear.addEventListener("click", handleClick);
+let totalPrice;
 
-// function handleClick() {
-//     localStorage.removeItem(PRODUCT_LS_KEY);
-//     window.location.href = "../index.html"
-// }
+
+if(products.length){
+    selectors.clear.hidden = false;
+    totalPrice = products.reduce((acc, {qty, price}) => acc += qty * price, 0);
+
+}
+
+selectors.totalPrice.textContent = totalPrice ? `Total price: ${totalPrice}` :  "Your basket is empty";
+selectors.container.insertAdjacentHTML("beforeend", createMarkup(products));
+selectors.clear.addEventListener("click", handleClick);
+
+function handleClick(){
+    localStorage.removeItem(PRODUCT_LS_KEY);
+    window.location.href = "../index.html"
+}
